1336b7d030a68d2eb7c4ba6006a9c509d0e55438
Beverly Pham <beverly.pham@gmail.com>
2018-01-09 14:25:58 -0800

    added third question and console logs

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var userName = prompt('Hi, what is your name?');
+console.log('user\s name is ' + userName)
 alert('Hello ' + userName + '! Let\'s play a Game');
 
 var city = prompt('Do you think I live in Seattle? (y/n)').toLowerCase();
+console.log('For city, user entered: ' + city);
 if (city === 'y') {
   alert('Yes I do live in Seattle!');
 } else if (city === 'n') {
@@ -13,10 +15,21 @@ if (city === 'y') {
 }
 
 var pets = prompt('Do I have any pets? (Y/N)').toUpperCase();
+console.log('For pets, user entered: ' + pets);
 if (pets === 'Y') {
   alert('Yes, I have a dog named Whiskey!');
 } else if (pets === 'N') {
   alert('Sorry you are incorrect. I have a dog.');
 } else {
   alert('Please enter Y or N');
+}
+
+var car = prompt('Do I drive an awesome car? (YES/NO)').toUpperCase();
+console.log('For car, user entered: ' + car);
+if (car === 'YES') {
+  alert('Yes, I have a Honda Civic!');
+} else if (car === 'NO') {
+  alert('Sorry you guessed wrong.');
+} else {
+  alert('Please enter YES or NO');
 }
